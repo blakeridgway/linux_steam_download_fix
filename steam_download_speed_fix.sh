30a4392a0e45cd418e10a38273f1e6f4f3b70fe1
Blake Ridgway <blake@blakeridgway.dev>
2024-05-03 20:27:29 -0500

    (feat): First iteration of the script

---
+++ steam_download_speed_fix.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+config="steam_dev.cfg"
+
+echo "Running Steam download fix"
+echo "Checking for native install."
+
+
+if [ -d "$HOME/.steam/steam/" ]
+then
+		echo "Native Steam installation was detected. Do you want to continue?"
+		select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) cp "$config" "$HOME/.steam/steam/"; break;;
+        No ) exit;;
+    esac
+done
+	else
+		echo "Native Steam installation was not detected. Checking for Flatpak."
+if [ -d "$HOME/.var/app/com.valvesoftware.Steam/.steam/steam/" ]
+then
+		echo "Flatpak Steam install was detected. Do you want to continue with the fix?"
+		select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) cp "$config" "$HOME/.var/app/com.valvesoftware.Steam/.steam/steam/"; break;;
+        No ) exit;;
+    esac
+done
+	else
+		echo "No Steam installation was not detected"
+	fi
+fi
+
+
+
+# Flatpak Install
+#~/.var/app/com.valvesoftware.Steam/.steam/steam/steam_dev.cfg
+
+
+# Native Install
+#~/.steam/steam/steam_dev.cfg
